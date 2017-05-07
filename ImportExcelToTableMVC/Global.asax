883e51ee8384db3e6da904c2475f643d0e27e219
gianneobrero <gianneobrero@gmail.com>
2017-05-07 16:48:13 -0500

    Add project files.

---
+++ ImportExcelToTableMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImportExcelToTableMVC.MvcApplication" Language="C#" %>
---
+++ ImportExcelToTableMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImportExcelToTableMVC.Startup))]
+namespace ImportExcelToTableMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
